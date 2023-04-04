fff543e6d9b17e8c52734e55b0069fd36500c43f
Aram <arma-arman@mail.ru>
2023-04-04 19:52:40 +0400

    Styling of the section Picture, and styling a basic responsive

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,16 @@
-const menuMobile = document.querySelector(".menu__mobile");
-const navMobile = document.querySelector(".nav__mobile");
+// const menuMobile = document.querySelector(".menu__mobile");
+// const navMobile = document.querySelector(".nav__mobile");
 
-navMobile.addEventListener("click", () => {
-    menuMobile.classList.toggle("active");
-    menuMobile.classList.remove("inactive");
+// navMobile.addEventListener("click", () => {
+//     menuMobile.classList.toggle("active");
+//     menuMobile.classList.remove("inactive");
+// });
+
+window.addEventListener("DOMContentLoaded", () => {
+    const mobileMenuBtn = document.querySelector(".menu__list");
+    const nav = document.querySelector(".nav__right");
+
+    mobileMenuBtn.addEventListener("click", () => {
+        nav.classList.toggle("active__nav");
+    })
 });
